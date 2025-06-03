4c082c6772b7d457e02f65123978215deb4dc7f3
hunjoonrhee <hunjoonrhee90@gmail.com>
2025-06-03 21:31:52 +0200

    display movie list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,17 @@
-import { Component } from '@angular/core';
-import {MovieItemComponent} from './movie-item/movie-item.component';
-import {Movie} from './model/movie.model';
-
+import { Component, inject } from '@angular/core';
+import { MovieItemComponent } from './movie-item/movie-item.component';
+import { Movie } from './model/movie.model';
+import { MoviesService } from './services/movies.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
   templateUrl: 'app.component.html',
-  imports: [
-    MovieItemComponent
-  ]
+  imports: [MovieItemComponent],
 })
 export class AppComponent {
+  readonly moviesService = inject(MoviesService);
 
-  movie: Movie =  {
-    "id": "e80d5a37-620e-4be2-92b9-fb1f5262494f",
-    "title": "Harry Potter and the Philosopher's Stone",
-    "duration": 152,
-    "budget": 125,
-    "release_date": "2001-11-04"
-  };
+  readonly movies = this.moviesService.getMovies();
 }
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, Signal } from '@angular/core';
+import { Movie } from '../model/movie.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  protected httpClient = inject(HttpClient);
+
+  getMovies(): Signal<Movie[]> {
+    return toSignal(this.httpClient.get<Movie[]>('/movies'), {
+      initialValue: [],
+    });
+  }
+}

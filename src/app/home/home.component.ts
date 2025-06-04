8236d4e45d8f06fb28d8bb415c5752f08069e0ea
hunjoonrhee <hunjoonrhee90@gmail.com>
2025-06-04 21:17:23 +0200

    Add Filters

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,22 @@ import { MovieItemComponent } from '../movie-item/movie-item.component';
 import { FavoritesService } from '../services/favorites.service';
 import { MoviesService } from '../services/movies.service';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  of,
+  startWith,
+  switchMap,
+} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MovieItemComponent, AsyncPipe],
+  imports: [MovieItemComponent, AsyncPipe, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -17,8 +28,48 @@ export class HomeComponent {
   readonly favoritesService = inject(FavoritesService);
 
   readonly movies$ = this.moviesService.getMovies();
+  readonly titleControl = new FormControl<string>('');
+  readonly yearControl = new FormControl<string>('');
 
   isMovieFavorite(movie: Movie) {
     this.favoritesService.toggleFavorite(movie);
   }
+  constructor() {
+    this.filteredMovies$.subscribe((m) => console.log(m));
+  }
+
+  filteredMovies$ = combineLatest([
+    this.titleControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      distinctUntilChanged()
+    ),
+    this.yearControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      distinctUntilChanged()
+    ),
+  ]).pipe(
+    map(
+      ([title, year]) =>
+        [title?.trim(), year?.toString().trim()] as [string, string]
+    ),
+    switchMap(([title, year]) =>
+      this.movies$.pipe(
+        map((movies) => {
+          const hasTitle = title ? title.length > 0 : false;
+          const hasYear = year ? year.length > 0 : false;
+          console.log(title, year);
+          if (!hasTitle && !hasYear) {
+            return null;
+          }
+          return movies.filter(
+            (m) =>
+              m.title.toLowerCase().includes(title?.toLowerCase() || '') &&
+              m.release_date.includes(year || '')
+          );
+        })
+      )
+    )
+  );
 }
